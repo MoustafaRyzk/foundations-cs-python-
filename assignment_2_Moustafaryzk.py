8fdb0705f5fff0ff890d767d3ff3f306b6735d1d
Moustafa Ryzk <semoustafarizk@gmail.com>
2023-10-26 22:38:28 +0300

    Enter MenuFunction

---
+++ assignment_2_Moustafaryzk.py
@@ -0,0 +1,12 @@
+# assignment num 2
+
+
+def Menu():
+    print("1. Count Digits")
+    print("2. Find Max    ")
+    print("3.1. Count Tags")
+    print("3.2. Count Normalized Columns")
+    print("4. Exit")
+    print("---------------------------")
+    print("Enter a Choice:")
+

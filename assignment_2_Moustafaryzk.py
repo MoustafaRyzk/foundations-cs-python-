bc208d2b8c7c0eed9e926dccd0614a3d91c0eaf9
Moustafa Ryzk <semoustafarizk@gmail.com>
2023-10-27 17:15:07 +0300

    add Start_assignment() function&make some changes

--- assignment_2_Moustafaryzk.py
+++ assignment_2_Moustafaryzk.py
@@ -1,6 +1,12 @@
 # assignmentt num 2
 
+def GetDigit(text):
 
+    num = input(text)
+    while not num.isdigit():
+        num = input(text)
+    num = int(num)
+    return num
 def Menu():
     print("1. Count Digits")
     print("2. Find Max    ")
@@ -8,6 +14,7 @@ def Menu():
     print("3.2. Count Normalized Columns")
     print("4. Exit")
     print("---------------------------")
+
 
 def count_Digits(number):
     if number == 0:
@@ -25,4 +32,42 @@ def get_max(lst):
             return lst[0]
         else:
             return sub_max
-    
+
+
+def Start_assignment():
+
+        Menu()
+
+        user_Choice=input("Please Enter your choice:")
+        while not ((user_Choice.isdigit()) and ((user_Choice =="1") or (user_Choice =="2") or (user_Choice =="3") or (user_Choice =="4"))):
+            user_Choice = input("Please Enter your choice:")
+
+        user_Choice=int(user_Choice)
+
+        if user_Choice==1:
+            num = input("Please enter a number:")
+            while not ((num[0] == "-" and num[1:].isdigit()) or num.isdigit()):
+                num = input("Please enter a number:")
+            num=int(num)
+
+            print(count_Digits(num))
+
+        if user_Choice==2:
+            lst = []
+            num_Item=GetDigit("Please Enter Number of Items")
+            for i in range(num_Item):
+                item = input("Enter item:")
+                while not item.isdigit():
+                    item = input("Enter item:")
+                item = int(item)
+                lst.append(item)
+            print("max item = ", get_max(lst))
+
+
+      
+
+
+        if user_Choice==4:
+            quit()
+
+

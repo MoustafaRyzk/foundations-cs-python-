97a6035fcce34223f8f57f88ae794819938f0331
Moustafa Ryzk <semoustafarizk@gmail.com>
2023-11-04 02:40:50 +0200

    add getStudentList function()

--- assignment_3_MoustafaRyznk.py
+++ assignment_3_MoustafaRyznk.py
@@ -9,3 +9,44 @@ def menu():
 8. Get Top Performers
 9. Exit
 - - - - - - - - - - - - - - -""")
+    
+def getStudentList():
+    student_Lst=[]
+    number_Of_Students=input("Please Enter Number Of Students:")
+    while not number_Of_Students.isnumeric() or number_Of_Students=="0":
+        number_Of_Students = input("Please Enter Number Of Students:")
+    number_Of_Students=int(number_Of_Students)
+    for i in range(number_Of_Students):
+        student_Dictionary=dict()
+
+
+        print("Please Enter ID of student", i + 1, ":", end=" ")
+        ID = input()
+        while not ID.isnumeric():
+            ID = input("Please Enter ID of student:")
+        ID=int(ID)
+        student_Dictionary["ID"] = ID
+
+        print("Please Enter Name of student", i+1, ":", end=" ")
+        name=input()
+        student_Dictionary["Name"]=name
+
+        print("Please Enter student", i+1, "age:", end=" ")
+        age = input()
+        while not age.isnumeric():
+            age = input()
+        age=int(age)
+        student_Dictionary["Age"] = age
+
+        print("Please Enter student", i+1, "major: ", end="")
+        major = input()
+        student_Dictionary["Major"] = major
+
+        print("Please Enter student", i+1, "GPA: ", end="")
+        GPA = input()
+        while not GPA.isnumeric():
+            GPA = input("Please Enter student GPA")
+        GPA=float(GPA)
+        student_Dictionary["GPA"] = GPA
+        student_Lst.append(student_Dictionary)
+    return student_Lst

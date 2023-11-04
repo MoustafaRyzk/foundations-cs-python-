88d3ca9deb36e18fc39ba36d801ca13cc0deebee
Moustafa Ryzk <semoustafarizk@gmail.com>
2023-11-04 02:40:11 +0200

    add menu function()

---
+++ assignment_3_MoustafaRyznk.py
@@ -0,0 +1,11 @@
+def menu():
+    print("""1. Get Student by ID
+2. Get All Students
+3. Get Students by Major
+4. Add Student
+5. Find Common Majors
+6. Delete Student
+7. Calculate Average GPA
+8. Get Top Performers
+9. Exit
+- - - - - - - - - - - - - - -""")

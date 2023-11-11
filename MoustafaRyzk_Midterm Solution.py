c75f2a23c999d94a2ee8058d1d4314842b144b3f
Moustafa Ryzk <semoustafarizk@gmail.com>
2023-11-11 20:48:29 +0200

    Update MoustafaRyzk_Midterm Solution.py
    add display menu

--- MoustafaRyzk_Midterm Solution.py
+++ MoustafaRyzk_Midterm Solution.py
@@ -22,3 +22,19 @@ def getPostiveIntNumFrom_to(text_Message, hint, fr_om, to):
 
         print(hint)
 #----------------------------------------------------------------#
+
+def displayMenu():
+    print("\n####################")
+    print("#       Menu       #")
+    print("####################")
+    print("1. Open Tab")
+    print("2. Close Tab")
+    print("3. Switch Tab")
+    print("4. Display All Tabs")
+    print("5. Open Nested Tab")
+    print("6. Clear All Tabs")
+    print("7. Save Tabs")
+    print("8. Import Tabs")
+    print("9. Exit")
+    print("####################")
+    print()
